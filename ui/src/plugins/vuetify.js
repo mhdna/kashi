53b361d191ef086fa8c6a871ea7d7bfa0fd42f00
Mahdi <mhdna@outlook.com>
2026-03-16 02:07:52 +0200

    chore(ui): add vuetify extra components

--- ui/src/plugins/vuetify.js
+++ ui/src/plugins/vuetify.js
@@ -7,6 +7,13 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import {
+  VFileUpload,
+  VFileUploadDropzone,
+  VFileUploadItem,
+  VFileUploadList,
+} from 'vuetify/labs/VFileUpload'
+import { VIconBtn } from 'vuetify/labs/VIconBtn'
 
 // Composables
 import { createVuetify } from 'vuetify'
@@ -15,5 +22,12 @@ import { createVuetify } from 'vuetify'
 export default createVuetify({
   theme: {
     defaultTheme: 'system',
+  },
+  components: {
+    VIconBtn,
+    VFileUpload,
+    VFileUploadDropzone,
+    VFileUploadItem,
+    VFileUploadList,
   },
 })
