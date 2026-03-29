8a98b2f1ec723d5f3cd64240a03f368d6aa55ab4
Mahdi <mhdna@outlook.com>
2026-03-29 12:35:59 +0300

    feat(db): add purchases table

---
+++ db/migrations/000018_create_purchases_table.down.sql
@@ -0,0 +1,2 @@
+drop table if exists purchase_items;
+drop table if exists purchases;
---
+++ db/migrations/000018_create_purchases_table.up.sql
@@ -0,0 +1,19 @@
+create table if not exists purchases (
+    id bigserial primary key,
+    supplier_id bigint not null references suppliers(id),
+    created_at timestamp(0) WITH time zone NOT NULL DEFAULT NOW()
+);
+
+create table if not exists purchase_items (
+    id bigserial primary key,
+    purchase_id bigint references purchases(id) on delete cascade,
+    product_id bigint references products(id),
+    asset_id bigint references assets(id),
+    quantity bigint not null,
+    unit_price numeric(12,2) not null default 0,
+    check (
+        (product_id is not null and asset_id is null) or
+        (product_id is null and asset_id is not null)
+    ),
+    unique (purchase_id, product_id, asset_id)
+);
