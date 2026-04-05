dcc534e86513f39c9928390622c0a7ed298cbb61
Mahdi <mhdna@outlook.com>
2026-04-05 14:58:06 +0300

    feat(db): add password_chaged_at field

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -2,10 +2,11 @@ CREATE EXTENSION IF NOT EXISTS citext;
 
 create table if not exists users (
     id bigserial primary key,
-    created_at timestamp(0) with time zone not null default now(),
     name text not null,
     email citext unique not null,
     password_hash bytea not null,
+    password_changed_at timestamp(0) with time zone not null default '0001-01-01 00:00:00+00',
     activated bool not null,
-    version int not null default 1
+    version int not null default 1,
+    created_at timestamp(0) with time zone not null default now(),
 );
