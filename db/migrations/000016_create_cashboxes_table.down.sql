5393f9e914be4c075ec8bb7d695027435a6e64bd
Mahdi <mhdna@outlook.com>
2026-04-08 16:45:45 +0300

    feat(db): add cashbox shifts

--- db/migrations/000016_create_cashboxes_table.down.sql
+++ db/migrations/000016_create_cashboxes_table.down.sql
@@ -1,1 +1,3 @@
+drop table if exists cashbox_shifts;
+drop table if exists shifts;
 drop table if exists cashboxes;
--- db/migrations/000016_create_cashboxes_table.up.sql
+++ db/migrations/000016_create_cashboxes_table.up.sql
@@ -4,3 +4,16 @@ create table if not exists cashboxes (
     is_active boolean not null default true,
     created_at timestamp(0) WITH time zone NOT NULL DEFAULT NOW()
 );
+
+create table if not exists shifts (
+    id bigserial primary key,
+    opening_balance numeric(12, 4) not null,
+    current_balance numeric(12, 4) not null,
+    opening_date_time timestamp(0) WITH time zone NOT NULL DEFAULT NOW(),
+    closing_date_time timestamp(0) WITH time zone
+);
+
+create table if not exists cashbox_shifts (
+    cashbox_id bigint not null references cashboxes(id),
+    shift_id bigint not null references shifts(id)
+);
