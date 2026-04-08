63416608705c1e175c62130a21471c67c31a7207
Mahdi <mhdna@outlook.com>
2026-04-08 11:38:26 +0300

    feat(db): add initial expenses table

---
+++ db/migrations/000020_create_expenses_table.down.sql
@@ -0,0 +1,8 @@
+create table if not exists expenses (
+    id bigserial primary key,
+    description text not null,
+    amount numeric(12,4) not null,
+    currency_id bigint not null references currencies(id),
+    cashbox_id bigint not null references cashboxes(id),
+    created_at timestamp(0) WITH time zone NOT NULL DEFAULT NOW()
+);
