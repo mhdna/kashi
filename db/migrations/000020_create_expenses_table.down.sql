9f35ad59a4231d15e33fc165517911d4b85f5e24
Mahdi <mhdna@outlook.com>
2026-04-08 16:47:33 +0300

    fix(db): flip up & down for expenses migration

--- db/migrations/000020_create_expenses_table.down.sql
+++ db/migrations/000020_create_expenses_table.down.sql
@@ -1,8 +1,1 @@
-create table if not exists expenses (
-    id bigserial primary key,
-    description text not null,
-    amount numeric(12,4) not null,
-    currency_id bigint not null references currencies(id),
-    cashbox_id bigint not null references cashboxes(id),
-    created_at timestamp(0) WITH time zone NOT NULL DEFAULT NOW()
-);
+drop table if exists expenses;
--- db/migrations/000020_create_expenses_table.up.sql
+++ db/migrations/000020_create_expenses_table.up.sql
@@ -1,1 +1,7 @@
-drop table if exists expenses;
+create table if not exists expenses (
+    id bigserial primary key,
+    description text not null,
+    amount numeric(12,4) not null,
+    currency_id bigint not null references currencies(id),
+    created_at timestamp(0) WITH time zone NOT NULL DEFAULT NOW()
+);
