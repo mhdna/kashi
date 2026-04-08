ef357cc2f71c26de9bfce3d583fa6d4b58181d39
Mahdi <mhdna@outlook.com>
2026-04-08 17:02:27 +0300

    feat(db): add expenses sql queries

---
+++ db/query/expense.sql
@@ -0,0 +1,19 @@
+-- name: CreateExpense :one
+INSERT INTO expenses (
+  description,
+  amount,
+  currency_id
+) 
+VALUES ( $1, $2, $3 )
+RETURNING *;
+
+-- name: GetExpense :one
+SELECT * FROM expenses
+WHERE id = $1 LIMIT 1;
+
+-- name: ListExpenses :many
+SELECT * FROM expenses
+WHERE id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
