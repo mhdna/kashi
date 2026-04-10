243df3204f122c27d3a36f58b18aee47cc1cbb23
Mahdi <mhdna@outlook.com>
2026-04-10 14:41:48 +0300

    test: add shift tests

---
+++ db/sqlc/shift_test.go
@@ -0,0 +1,107 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/mhdna/kashi/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomShift(t *testing.T) Shift {
+	cashbox := createRandomCashbox(t)
+
+	arg := CreateShiftParams{
+		CashboxID:           cashbox.ID,
+		TotalOpeningBalance: util.RandomMoneyAmount(),
+		TotalBalance:        util.RandomMoneyAmount(),
+	}
+
+	shift, err := testQueries.CreateShift(context.Background(), arg)
+	require.NoError(t, err)
+	require.Equal(t, shift.CashboxID, arg.CashboxID)
+	require.Equal(t, shift.TotalOpeningBalance, arg.TotalOpeningBalance)
+	require.Equal(t, shift.TotalBalance, arg.TotalBalance)
+
+	return shift
+}
+
+func TestCreateShift(t *testing.T) {
+	createRandomShift(t)
+}
+
+func TestGetShift(t *testing.T) {
+	shift1 := createRandomShift(t)
+
+	shift2, err := testQueries.GetShift(context.Background(), shift1.ID)
+	require.NoError(t, err)
+	require.Equal(t, shift1.ID, shift2.ID)
+	require.Equal(t, shift1.CashboxID, shift2.CashboxID)
+	require.Equal(t, shift1.TotalOpeningBalance, shift2.TotalOpeningBalance)
+	require.Equal(t, shift1.TotalBalance, shift2.TotalBalance)
+
+	require.WithinDuration(t, shift1.OpeningDateTime, shift2.OpeningDateTime, time.Second)
+	require.WithinDuration(t, shift1.ClosingDateTime.Time, shift2.ClosingDateTime.Time, time.Second)
+}
+
+func TestListShifts(t *testing.T) {
+	for range 10 {
+		createRandomShift(t)
+	}
+
+	arg := ListShiftsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	shifts, err := testQueries.ListShifts(context.Background(), arg)
+	require.NoError(t, err)
+	for shift := range shifts {
+		require.NotEmpty(t, shift)
+	}
+}
+
+func TestUpdateShiftBalance(t *testing.T) {
+	shift := createRandomShift(t)
+
+	currentBalance := shift.TotalBalance
+
+	arg := UpdateShiftBalanceParams{
+		ID:           shift.ID,
+		TotalBalance: currentBalance + util.RandomMoneyAmount(),
+	}
+
+	err := testQueries.UpdateShiftBalance(context.Background(), arg)
+	require.NoError(t, err)
+
+	shift2, err := testQueries.GetShift(context.Background(), shift.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	require.NoError(t, err)
+	require.Equal(t, shift2.TotalBalance, arg.TotalBalance)
+}
+
+func TestCloseShift(t *testing.T) {
+	shift := createRandomShift(t)
+
+	arg := CloseShiftParams{
+		ID:              shift.ID,
+		ClosingDateTime: sql.NullTime{Time: time.Now(), Valid: true},
+		IsClosed:        true,
+	}
+
+	err := testQueries.CloseShift(context.Background(), arg)
+	require.NoError(t, err)
+
+	shift2, err := testQueries.GetShift(context.Background(), shift.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	require.NoError(t, err)
+	require.Equal(t, shift2.IsClosed, arg.IsClosed)
+	require.WithinDuration(t, shift2.ClosingDateTime.Time, arg.ClosingDateTime.Time, time.Second)
+}
