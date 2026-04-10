70bb0ce867a3f339bcc6552d1b8092a1fa9442cb
Mahdi <mhdna@outlook.com>
2026-04-10 13:09:15 +0300

    update math utils to match the db model for amount (int64 instead of string)

--- util/math.go
+++ util/math.go
@@ -1,21 +1,6 @@
 package util
 
-import "strconv"
-
-func CalculateNetAmount(amount string, discount int16) (string, error) {
-	amt, err := strconv.ParseFloat(amount, 10)
-	if err != nil {
-		return "", err
-	}
-	d := int16(0)
-	netAmount := amt * (100 - float64(d)) / 100
-	return strconv.FormatFloat(netAmount, 'f', 0, 64), nil
-}
-
-func NegateAmount(amount string) (string, error) {
-	amt, err := strconv.ParseInt(amount, 10, 64)
-	if err != nil {
-		return "", err
-	}
-	return strconv.FormatInt(-amt, 10), nil
+func CalculateNetAmount(amount int64, discount int16) (int64, error) {
+	netAmount := amount * (100 - int64(discount)) / 100
+	return netAmount, nil
 }
