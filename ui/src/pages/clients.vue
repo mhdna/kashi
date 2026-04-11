e95678518faf72d5df361be8a8421f27d0da3345
Mahdi <mhdna@outlook.com>
2026-04-11 20:28:30 +0300

    feat(ui): add clients table

---
+++ ui/src/pages/clients.vue
@@ -0,0 +1,14 @@
+<template>
+    <ServerSideTable :header="headers" :apiURL="apiURL" rootKey="clients" />
+</template>
+
+<script setup>
+const apiURL = 'http://localhost:4123/clients'
+const headers = ref([
+    { title: 'ID', key: 'id', align: 'start' },
+    { title: 'Name', key: 'name', align: 'start' },
+    { title: 'Phone', key: 'phone', align: 'start' },
+    { title: 'Loyalty Points', key: 'loyalty_points', align: 'end' },
+    { title: 'Created At', key: 'created_at', align: 'end' },
+])
+</script>
