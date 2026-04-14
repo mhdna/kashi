31ccc68eaa3ecc93fdc338e7ef3c980827c1dbe2
Mahdi <mhdna@outlook.com>
2026-04-14 09:55:24 +0300

    feat(ui): add login page & layout

---
+++ ui/src/pages/Login.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import LoginForm from '@/components/Forms/LoginForm.vue';
+</script>
+
+<template>
+    <LoginForm />
+</template>
+
+<route lang="yaml">
+meta:
+  layout: login
+</route>
