71846ee5f907dc0d674c5654d8188c6eae7bb1c1
Mahdi <mhdna@outlook.com>
2026-04-18 13:38:24 +0300

    test: test over multiple iterations and pass idx to retain order

--- db/sqlc/tx_product_test.go
+++ db/sqlc/tx_product_test.go
@@ -11,49 +11,74 @@ import (
 
 func TestCreateProductTx(t *testing.T) {
 	store := NewStore(testDB)
-	name := util.RandomString(20)
-	code := util.RandomString(8)
-	description := util.RandomString(200)
-	price := util.RandomNumber()
-	discount := util.RandomNumber()
-	attributeValues := createRandomAttributeValues(t)
+
+	n := 5
+	names := make([]string, n)
+	codes := make([]string, n)
+	descriptions := make([]string, n)
+	prices := make([]int64, n)
+	discounts := make([]int16, n)
+	attributeValuesArray := make([][]AttributesValue, n)
+
+	for i := range n {
+		names[i] = util.RandomString(20)
+		codes[i] = util.RandomString(8)
+		descriptions[i] = util.RandomString(200)
+		prices[i] = util.RandomNumber()
+		discounts[i] = util.RandomDiscount()
+		attributeValuesArray[i] = createRandomAttributeValues(t)
+	}
 
 	errs := make(chan error)
-	results := make(chan CreateProductTxResult)
 
-	go func() {
-		res, err := store.CreateProductTx(context.Background(), CreateProductTxParams{
-			Code:            code,
-			Name:            name,
-			Description:     description,
-			Price:           price,
-			Discount:        discount,
-			AttributeValues: attributeValues,
-		})
-		errs <- err
-		results <- res
-	}()
+	type indexedResult struct {
+		res CreateProductTxResult
+		idx int
+	}
 
-	err := <-errs
-	require.NoError(t, err)
+	results := make(chan indexedResult)
 
-	res := <-results
+	for i := range n {
+		go func(idx int) {
+			res, err := store.CreateProductTx(context.Background(), CreateProductTxParams{
+				Code:            codes[idx],
+				Name:            names[idx],
+				Description:     descriptions[idx],
+				Price:           prices[idx],
+				Discount:        discounts[idx],
+				AttributeValues: attributeValuesArray[idx],
+			})
 
-	resProduct := res.Product
-	require.NotEmpty(t, resProduct)
-	require.Equal(t, code, resProduct.Code)
-	require.Equal(t, description, resProduct.Description)
-	require.Equal(t, discount, resProduct.Discount)
-	require.NotZero(t, resProduct.ID)
-	require.NotZero(t, resProduct.CreatedAt)
+			errs <- err
+			results <- indexedResult{idx: idx, res: res}
+		}(i)
+	}
 
-	resProductAttributes := res.ProductAttributes
-	for _, attributeValue := range attributeValues {
-		for _, resProductAttribute := range resProductAttributes {
-			if resProductAttribute.Attribute == attributeValue.Attribute {
-				fmt.Println(">> tx product attribute:", resProductAttribute.Attribute, attributeValue.Attribute, resProductAttribute.AttributeValueID, attributeValue.ID)
-				require.Equal(t, attributeValue.Attribute, resProductAttribute.Attribute)
-				require.Equal(t, attributeValue.ID, resProductAttribute.AttributeValueID)
+	for range n {
+		err := <-errs
+		require.NoError(t, err)
+
+		indexedRes := <-results
+		res := indexedRes.res
+		idx := indexedRes.idx
+
+		resProduct := res.Product
+		require.NotEmpty(t, resProduct)
+		require.Equal(t, codes[idx], resProduct.Code)
+		require.Equal(t, descriptions[idx], resProduct.Description)
+		require.Equal(t, discounts[idx], resProduct.Discount)
+		require.NotZero(t, resProduct.ID)
+		require.NotZero(t, resProduct.CreatedAt)
+		fmt.Println(">> tx product:", res.Product.Code)
+
+		resProductAttributes := res.ProductAttributes
+		for _, attributeValue := range attributeValuesArray[idx] {
+			for _, resProductAttribute := range resProductAttributes {
+				if resProductAttribute.Attribute == attributeValue.Attribute {
+					fmt.Println(">> attribute:", resProductAttribute.Attribute, attributeValue.Attribute, resProductAttribute.AttributeValueID, attributeValue.ID)
+					require.Equal(t, attributeValue.Attribute, resProductAttribute.Attribute)
+					require.Equal(t, attributeValue.ID, resProductAttribute.AttributeValueID)
+				}
 			}
 		}
 	}
