890edca3f3ba0b87536a8c799cea63d0ff5fdd9d
Mahdi <mhdna@outlook.com>
2026-04-18 13:44:22 +0300

    api: append attributes to each product GET request

--- api/product.go
+++ api/product.go
@@ -8,12 +8,25 @@ import (
 	db "github.com/mhdna/kashi/db/sqlc"
 )
 
+type Attributes struct {
+	Category    string `json:"category"`
+	SubCategory string `json:"subcategory"`
+	Brand       string `json:"brand"`
+	Kind        string `json:"kind"`
+	Type        string `json:"type"`
+	Unit        string `json:"unit"`
+	Year        string `json:"year"`
+	Season      string `json:"season"`
+	Origin      string `json:"origin"`
+}
+
 type createProductRequest struct {
-	Code        string `json:"code" binding:"required"`
-	Name        string `json:"name" binding:"required"`
-	Description string `json:"description"`
-	Price       int64  `json:"price"`
-	Discount    int64  `json:"discount"`
+	Code        string     `json:"code" binding:"required"`
+	Name        string     `json:"name" binding:"required"`
+	Description string     `json:"description"`
+	Price       int64      `json:"price"`
+	Discount    int16      `json:"discount"`
+	Attributes  Attributes `json:"attributes"`
 }
 
 func (server *Server) createProduct(ctx *gin.Context) {
@@ -23,21 +36,32 @@ func (server *Server) createProduct(ctx *gin.Context) {
 		return
 	}
 
-	arg := db.CreateProductParams{
-		Code:        req.Code,
-		Name:        req.Name,
-		Description: req.Description,
-		Price:       req.Price,
-		Discount:    req.Discount,
+	attributeValues := []db.AttributesValue{
+		{Attribute: "category", Value: req.Attributes.Category},
+		{Attribute: "subcategory", Value: req.Attributes.SubCategory},
+		{Attribute: "brand", Value: req.Attributes.Brand},
+		{Attribute: "kind", Value: req.Attributes.Kind},
+		{Attribute: "type", Value: req.Attributes.Type},
+		{Attribute: "unit", Value: req.Attributes.Unit},
+		{Attribute: "year", Value: req.Attributes.Year},
+		{Attribute: "season", Value: req.Attributes.Season},
+		{Attribute: "origin", Value: req.Attributes.Origin},
 	}
 
-	product, err := server.store.CreateProduct(ctx, arg)
+	createProductArg := db.CreateProductTxParams{
+		Code:            req.Code,
+		Name:            req.Name,
+		Description:     req.Description,
+		Price:           req.Price,
+		Discount:        req.Discount,
+		AttributeValues: attributeValues,
+	}
+
+	product, err := server.store.CreateProductTx(ctx, createProductArg)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-		
-
 	ctx.JSON(http.StatusOK, product)
 }
 
@@ -63,7 +87,17 @@ func (server *Server) getProduct(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, product)
+	attributes, err := server.store.GetProductAttributes(ctx, req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// TODO: use envelop
+	ctx.JSON(http.StatusOK, gin.H{
+		"product":    product,
+		"attributes": attributes,
+	})
 }
 
 type listProductRequest struct {
