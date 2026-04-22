d85c3ed751feaf5822e0dc8371a209cc1a4564be
Mahdi <mhdna@outlook.com>
2026-04-22 12:32:18 +0300

    test: add a value to all available attributes per product

--- db/sqlc/product_attributes_test.go
+++ db/sqlc/product_attributes_test.go
@@ -8,46 +8,50 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func createRandomProductAttribute(t *testing.T) ProductsAttribute {
+func createRandomProductAttributes(t *testing.T) []ProductsAttribute {
 	product := createRandomProduct(t)
-	attribute_value := createRandomAttributeValue(t)
+	attributeValues := createRandomAttributeValues(t)
+	productAttributes := []ProductsAttribute{}
 
-	arg := CreateProductAttributeParams{
-		ProductID:        product.ID,
-		AttributeValueID: attribute_value.ID,
-		Attribute:        attribute_value.Attribute,
+	for _, a := range attributeValues {
+		addAttributeArg := CreateProductAttributeParams{
+			ProductID:        product.ID,
+			Attribute:        a.Attribute,
+			AttributeValueID: a.ID,
+		}
+		productAttribute, err := testQueries.CreateProductAttribute(context.Background(), addAttributeArg)
+		require.NoError(t, err)
+
+		productAttributes = append(productAttributes, productAttribute)
 	}
 
-	product_attribute, err := testQueries.CreateProductAttribute(context.Background(), arg)
-	require.NoError(t, err)
-	require.Equal(t, product.ID, product_attribute.ProductID)
-	require.Equal(t, attribute_value.Attribute, product_attribute.Attribute)
-	require.Equal(t, attribute_value.ID, product_attribute.AttributeValueID)
-
-	return product_attribute
+	return productAttributes
 }
 
 func TestCreateProductAttribute(t *testing.T) {
-	createRandomProductAttribute(t)
+	createRandomProductAttributes(t)
 }
 
 func TestGetProductAttribute(t *testing.T) {
-	product_attribute := createRandomProductAttribute(t)
+	productAttributes := createRandomProductAttributes(t)
 
-	arg := GetProductAttributeValueParams{
-		ProductID: product_attribute.ProductID,
-		Attribute: product_attribute.Attribute,
+	for _, productAttribute := range productAttributes {
+		arg := GetProductAttributeValueParams{
+			ProductID: productAttribute.ProductID,
+			Attribute: productAttribute.Attribute,
+		}
+
+		productAttribute2, err := testQueries.GetProductAttributeValue(context.Background(), arg)
+		require.NoError(t, err)
+		require.Equal(t, productAttribute.ProductID, productAttribute2.ProductID)
+		require.Equal(t, productAttribute.Attribute, productAttribute2.Attribute)
+		require.Equal(t, productAttribute.AttributeValueID, productAttribute2.AttributeValueID)
 	}
-
-	product_attribute2, err := testQueries.GetProductAttributeValue(context.Background(), arg)
-	require.NoError(t, err)
-	require.Equal(t, product_attribute.ProductID, product_attribute2.ProductID)
-	require.Equal(t, product_attribute.Attribute, product_attribute2.Attribute)
 }
 
 func TestListProductAttributes(t *testing.T) {
 	for range 10 {
-		createRandomProductAttribute(t)
+		createRandomProductAttributes(t)
 	}
 
 	limit := 5
@@ -67,28 +71,30 @@ func TestListProductAttributes(t *testing.T) {
 	}
 }
 
-func TestUpdateProductAttributes(t *testing.T) {
-	productAttribute := createRandomProductAttribute(t)
+func TestUpdateProductAttribute(t *testing.T) {
+	productAttributes := createRandomProductAttributes(t)
 
 	attributesLength := int64(5)
 	for range attributesLength {
-		createRandomAttributeValue(t)
+		createRandomAttributeValues(t)
 	}
 
-	arg := UpdateProductAttributeParams{
-		ProductID:        productAttribute.ProductID,
-		Attribute:        productAttribute.Attribute,
-		AttributeValueID: util.RandomInt(1, attributesLength),
-	}
-	err := testQueries.UpdateProductAttribute(context.Background(), arg)
-	require.NoError(t, err)
+	for _, productAttribute := range productAttributes {
+		arg := UpdateProductAttributeParams{
+			ProductID:        productAttribute.ProductID,
+			Attribute:        productAttribute.Attribute,
+			AttributeValueID: util.RandomInt(1, attributesLength),
+		}
+		err := testQueries.UpdateProductAttribute(context.Background(), arg)
+		require.NoError(t, err)
 
-	arg2 := GetProductAttributeValueParams{
-		ProductID: productAttribute.ProductID,
-		Attribute: productAttribute.Attribute,
+		arg2 := GetProductAttributeValueParams{
+			ProductID: productAttribute.ProductID,
+			Attribute: productAttribute.Attribute,
+		}
+		productAttribute2, err := testQueries.GetProductAttributeValue(context.Background(), arg2)
+		require.Equal(t, productAttribute2.ProductID, arg.ProductID)
+		require.Equal(t, productAttribute2.Attribute, arg.Attribute)
+		require.Equal(t, productAttribute2.AttributeValueID, arg.AttributeValueID)
 	}
-	productAttribute2, err := testQueries.GetProductAttributeValue(context.Background(), arg2)
-	require.Equal(t, productAttribute2.ProductID, arg.ProductID)
-	require.Equal(t, productAttribute2.Attribute, arg.Attribute)
-	require.Equal(t, productAttribute2.AttributeValueID, arg.AttributeValueID)
 }
