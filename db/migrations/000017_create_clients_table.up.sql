cb9f23b323b10896b399c21ab970659e7bec9642
Mahdi <mhdna@outlook.com>
2026-05-04 22:15:24 +0300

    db: phone should be unique

--- db/migrations/000017_create_clients_table.up.sql
+++ db/migrations/000017_create_clients_table.up.sql
@@ -1,7 +1,7 @@
 create table if not exists clients (
     id bigserial primary key,
     name text not null,
-    phone text not null,
+    phone text not null unique,
     total_loyalty_points bigint not null default 0,
     valid_loyalty_points bigint not null default 0,
     created_at timestamp(0) WITH time zone NOT NULL DEFAULT NOW()
