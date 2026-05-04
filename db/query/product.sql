34905b2ca2961e8f27cc4bdb406bf87e95d0c17f
Mahdi <mhdna@outlook.com>
2026-05-04 21:31:52 +0300

    db(products): add price list queries

--- db/query/product.sql
+++ db/query/product.sql
@@ -31,3 +31,35 @@ WHERE id = $1;
 DELETE FROM products
 WHERE id = $1;
 
+
+-- name: CreatePriceList :one
+INSERT INTO price_lists (
+  name,
+  code,
+  valid_from,
+  valid_to
+) VALUES (
+    $1, $2, $3, $4
+) RETURNING *;
+
+-- name: UpdatePriceList :exec
+UPDATE price_lists
+  SET name = $2,
+  code = $3,
+  valid_from = $4,
+  valid_to = $5
+WHERE id = $1;
+
+-- name: CreateProductPrice :one
+INSERT INTO price_list_items (
+  price_list_id,
+  product_id,
+  price
+) VALUES (
+    $1, $2, $3
+) RETURNING *;
+
+-- name: UpdateProductPrice :exec
+UPDATE price_list_items
+  SET price = $3
+WHERE product_id = $1 AND price_list_id = $2;
