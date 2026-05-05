d5efeacfb789444f1af7dedfcdc6af418d34db72
Mahdi <mhdna@outlook.com>
2026-05-05 18:57:57 +0300

    db(products): add discount_lists

--- db/migrations/000004_create_products_table.up.sql
+++ db/migrations/000004_create_products_table.up.sql
@@ -4,8 +4,6 @@ CREATE TABLE IF NOT EXISTS products (
   name text NOT NULL,
   description text not null,
   is_active bool not null DEFAULT true,
-  version int NOT NULL DEFAULT 1,
-  discount SMALLINT not null CHECK (discount >= 0 AND discount <= 100),
   created_at timestamp(0) WITH time zone NOT NULL DEFAULT NOW()
 );
 
@@ -26,5 +24,25 @@ create table price_list_items (
   product_id bigint references products(id),
   price bigint not null,
   
+  primary key (price_list_id, product_id)
+);
+
+create table discount_lists (
+  id bigserial primary key,
+  name text not null,
+  code text not null,
+  is_active boolean not null default true,
+  
+  valid_from timestamp,
+  valid_to timestamp,
+  
+  created_at timestamp with time zone default now()
+);
+
+create table discount_list_items (
+  discount_list_id bigint references discount_lists(id),
+  product_id bigint references products(id),
+  discount SMALLINT not null CHECK (discount >= 0 AND discount <= 100),
+
   primary key (price_list_id, product_id)
 );
