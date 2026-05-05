85f802e4bb597e412dfd4b968de1134635949ce7
Mahdi <mhdna@outlook.com>
2026-05-05 19:25:43 +0300

    db: add default price list and default discount list fields

--- db/migrations/000004_create_products_table.up.sql
+++ db/migrations/000004_create_products_table.up.sql
@@ -11,12 +11,18 @@ create table price_lists (
   id bigserial primary key,
   name text not null,
   is_active boolean not null default true,
+  is_default boolean not null default false,
   
-  valid_from timestamp,
-  valid_to timestamp,
+  valid_from timestamp not null,
+  valid_to timestamp not null,
   
   created_at timestamp with time zone default now()
 );
+
+-- Ensure there's only one default price_list
+CREATE UNIQUE INDEX only_one_default_price_list
+ON price_lists (is_default)
+WHERE is_default = true;
 
 create table price_list_items (
   price_list_id bigint references price_lists(id),
@@ -30,12 +36,18 @@ create table discount_lists (
   id bigserial primary key,
   name text not null,
   is_active boolean not null default true,
+  is_default boolean not null default false,
   
-  valid_from timestamp,
-  valid_to timestamp,
+  valid_from timestamp not null,
+  valid_to timestamp not null,
   
   created_at timestamp with time zone default now()
 );
+
+-- Ensure there's only one default discount list
+CREATE UNIQUE INDEX only_one_default_discount_list
+ON discount_lists (is_default)
+WHERE is_default = true;
 
 create table discount_list_items (
   discount_list_id bigint references discount_lists(id),
