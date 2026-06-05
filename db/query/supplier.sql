bdfb18245efdc218e15bffbc7dad3cc574961357
Mahdi <mhdna@outlook.com>
2026-06-05 14:07:38 +0300

    db(supplier): remove product queries (moved to purchase)

--- db/query/supplier.sql
+++ db/query/supplier.sql
@@ -14,29 +14,10 @@ RETURNING *;
 SELECT * FROM suppliers
 WHERE id = $1 LIMIT 1;
 
+-- TOOD: add UpdateSupplier
+
 -- name: ListSuppliers :many
 SELECT * FROM suppliers
 ORDER BY id
 LIMIT $1
 OFFSET $2;
-
--- name: AddSupplierProduct :one
-INSERT INTO product_suppliers (
-  product_id,
-  supplier_id
-) 
-VALUES ( $1, $2 )
-RETURNING *;
-
--- name: AddSupplierProductCost :one
-INSERT INTO product_supplier_costs (
-  product_supplier_id,
-  unit_cost,
-  currency_code
-) 
-VALUES ( $1, $2, $3 )
-RETURNING *;
-
--- name: DeleteSupplierProduct :exec
-delete from product_suppliers
-where id = $1;
