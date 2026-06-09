e035f5d11cb89252c8f9e0a9e8a1501593624480
Mahdi <mhdna@outlook.com>
2026-06-09 07:36:41 +0300

    db(users): add sqlc statement for users table

---
+++ db/query/user.sql
@@ -0,0 +1,28 @@
+-- name: CreateUser :one
+INSERT INTO users (
+  name,
+  email,
+  password_hash
+) VALUES ( $1, $2, $3 )
+RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users
+WHERE id = $1 LIMIT 1;
+
+-- name: ListUsers :many
+SELECT * FROM users
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateUser :exec
+UPDATE users
+  SET name = $2,
+  email = $3,
+  password_hash = $4
+WHERE id = $1;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
